a38febaf25758d240f5bd2a9fd9bf02fc2e2a715
Maicon Souza <maicon_souza09@hotmail.com>
2022-11-03 11:47:17 -0300

    full icon update

--- src/components/Sidebar/compSidebar/Nav.tsx
+++ src/components/Sidebar/compSidebar/Nav.tsx
@@ -1,4 +1,4 @@
-import { ChatCircle, Desktop, House, StackOverflowLogo, User } from "phosphor-react";
+import { FaHouseUser, FaUserAlt, FaStackOverflow, FaLaptopCode, FaComments } from 'react-icons/fa'
 import { NavLink } from "react-router-dom";
 import { ContainerNav } from "./Nav.styles";
 
@@ -7,28 +7,28 @@ export function Nav(){
         <ContainerNav>
             <ul>
                 <li>
-                    <NavLink to="/">
-                        <House size={23}/>
+                    <NavLink to="/" title="Home">
+                        <FaHouseUser size={18}/>
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/about-me">
-                        <User size={23}/>
+                    <NavLink to="/about-me" title="Quem sou">
+                        <FaUserAlt size={18}/>
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/stacks">
-                        <StackOverflowLogo size={23}/>
+                    <NavLink to="/stacks"title="Tecnologias">
+                        <FaStackOverflow size={18}/>
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/portfolio">
-                        <Desktop size={23}/>
+                    <NavLink to="/portfolio" title="Portfólio">
+                        <FaLaptopCode size={18}/>
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink to="/contact">
-                        <ChatCircle size={23}/>
+                    <NavLink to="/contact" title="Contato">
+                        <FaComments size={18}/>
                     </NavLink>
                 </li>
             </ul>
